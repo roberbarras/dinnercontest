ba6f9a26f2083bab0a6123a994dc70349318f895
roberbarras <roberbarras@gmail.com>
2020-01-16 19:25:50 +0100

    add group table to ddl

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -21,12 +21,24 @@ create table tokens
 		unique (access_name, token)
 );
 
+create table groups
+(
+    group_id     bigserial
+        constraint groups_pk
+            primary key,
+	group_name varchar
+);
+
 create unique index users_access_name_uindex
     on users (access_name);
 
+create unique index group_group_name_uindex
+    on groups (group_name);
+
 alter table tokens owner to postgres;
 
-alter table users
-    owner to postgres;
+alter table users owner to postgres;
+
+alter table groups owner to postgres;
 
 
