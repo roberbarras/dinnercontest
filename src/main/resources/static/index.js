7f4dda005255563b867b1346a8abcd8c5b0171af
roberbarras <roberbarras@gmail.com>
2020-01-19 00:42:39 +0100

    check and redirect in index

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,6 +1,28 @@
-if (localStorage.getItem("token") != null) {
-    //TODO token check
-    location.href = "/app.html";
-} else {
-    location.href = "/login.html";
+function checkLogin() {
+
+    class UserToken {
+        constructor(accessName, token) {
+            this.accessName = accessName;
+            this.token = token;
+        }
+    }
+
+    const userToken = new UserToken(localStorage.getItem("user"), localStorage.getItem("token"));
+
+    if (localStorage.getItem("token") != null && localStorage.getItem("user") != null) {
+        const request = new XMLHttpRequest();
+        request.open("POST", "http://localhost:8080/api/check-token");
+        request.setRequestHeader("Content-Type", "application/json");
+        request.send(JSON.stringify(userToken));
+
+        request.onload = function () {
+            if (request.status === 200) {
+                location.href = "/app.html";
+            } else {
+                location.href = "/login.html";
+            }
+        }
+    } else {
+        location.href = "/login.html"
+    }
 }
